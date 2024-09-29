eab498fb7801c5484bdad6c69656b3d35ab9df08
Comma Device <device@comma.ai>
2024-09-29 13:44:31 +0800

    long patch

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+export MAPBOX_TOKEN="pk.eyJ1IjoiMjk5Mjk5IiwiYSI6ImNsZ3V2Ymo4cTBrN2IzY2tsZzUzbGMybmwifQ.UmEHxJGkmirEPC4Oc-TZjg"
+export SKIP_FW_QUERY="1"
+export FINGERPRINT="TOYOTA_COROLLA_TSS2"
+export PASSIVE="0"
+
 exec ./launch_chffrplus.sh
