6041cec1a0c4e3b9e03eee9db694acffda9d4c62
golden.li <golden.li@smart.com>
2024-11-08 09:03:56 +0800

    fix crash of plannerd

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -6,35 +6,57 @@ from openpilot.common.swaglog import cloudlog
 from openpilot.selfdrive.controls.lib.ldw import LaneDepartureWarning
 from openpilot.selfdrive.controls.lib.longitudinal_planner import LongitudinalPlanner
 import cereal.messaging as messaging
+import os
+from openpilot.common.realtime import Ratekeeper
+
+
+def sim_main():
+    longitudinal_planner = LongitudinalPlanner(None)
+    rk = Ratekeeper(20)
+    while True:
+        longitudinal_planner.update_mock()
+        rk.keep_time()
 
 
 def main():
-  config_realtime_process(5, Priority.CTRL_LOW)
+    if os.getenv("NOBOARD") is not None:
+        sim_main()
 
-  cloudlog.info("plannerd is waiting for CarParams")
-  params = Params()
-  CP = messaging.log_from_bytes(params.get("CarParams", block=True), car.CarParams)
-  cloudlog.info("plannerd got CarParams: %s", CP.carName)
+    config_realtime_process(5, Priority.CTRL_LOW)
 
-  ldw = LaneDepartureWarning()
-  longitudinal_planner = LongitudinalPlanner(CP)
-  pm = messaging.PubMaster(['longitudinalPlan', 'driverAssistance'])
-  sm = messaging.SubMaster(['carControl', 'carState', 'controlsState', 'liveParameters', 'radarState', 'modelV2', 'selfdriveState'],
-                           poll='modelV2', ignore_avg_freq=['radarState'])
+    cloudlog.info("plannerd is waiting for CarParams")
+    params = Params()
 
-  while True:
-    sm.update()
-    if sm.updated['modelV2']:
-      longitudinal_planner.update(sm)
-      longitudinal_planner.publish(sm, pm)
+    CP = messaging.log_from_bytes(params.get("CarParams", block=True),
+                                  car.CarParams)
+    cloudlog.info("plannerd got CarParams: %s", CP.carName)
 
-      ldw.update(sm.frame, sm['modelV2'], sm['carState'], sm['carControl'])
-      msg = messaging.new_message('driverAssistance')
-      msg.valid = sm.all_checks(['carState', 'carControl', 'modelV2', 'liveParameters'])
-      msg.driverAssistance.leftLaneDeparture = ldw.left
-      msg.driverAssistance.rightLaneDeparture = ldw.right
-      pm.send('driverAssistance', msg)
+    ldw = LaneDepartureWarning()
+    longitudinal_planner = LongitudinalPlanner(CP)
+    pm = messaging.PubMaster(['longitudinalPlan', 'driverAssistance'])
+
+    sm = messaging.SubMaster([
+        'carControl', 'carState', 'controlsState', 'liveParameters',
+        'radarState', 'modelV2', 'selfdriveState'
+    ],
+                             poll='modelV2',
+                             ignore_avg_freq=['radarState'])
+
+    while True:
+        sm.update()
+        if sm.updated['modelV2']:
+            longitudinal_planner.update(sm)
+            longitudinal_planner.publish(sm, pm)
+
+            ldw.update(sm.frame, sm['modelV2'], sm['carState'],
+                       sm['carControl'])
+            msg = messaging.new_message('driverAssistance')
+            msg.valid = sm.all_checks(
+                ['carState', 'carControl', 'modelV2', 'liveParameters'])
+            msg.driverAssistance.leftLaneDeparture = ldw.left
+            msg.driverAssistance.rightLaneDeparture = ldw.right
+            pm.send('driverAssistance', msg)
 
 
 if __name__ == "__main__":
-  main()
+    main()
